9a394e8991a6b3709e538aa76b8364cfdcae7026
goisot <hugo.goisot@lyceeastier.com>
2024-03-14 09:06:55 +0100

    version de départ

---
+++ js/index.js
@@ -0,0 +1,9 @@
+document.onreadystatechange = function () {
+    console.log(document.readyState)
+    if(document.readyState == 'interactive') {
+        console.log("c'est ok")
+
+        let dd = new Date() ;
+        let df = new Date(Date.now())
+    }
+}
