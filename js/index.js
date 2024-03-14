fc67efcbeb07540118fb24fbafb6cb1fd1369191
goisot <hugo.goisot@lyceeastier.com>
2024-03-14 09:55:36 +0100

    version avec le js mais pas encore la différence de time

--- js/index.js
+++ js/index.js
@@ -1,10 +1,45 @@
 document.onreadystatechange = function () {
     console.log(document.readyState)
     if(document.readyState == 'interactive') {
-        console.log("c'est ok")
+        let dateIn;
+        let dateOut;
+        let tabDateIn;
+        let tabDateOut;
+        let flagDateIn = false;
+        let flagDateOut = false;
+        let deltaAnnee;
+        let deltaMois;
+        let deltaJour;
 
-        let dd = new Date() ;
-        let df = new Date(Date.now())
+        let timeIn;
+        document.getElementById("date_debut").onchange = function () {
+            dateIn = document.getElementById("date_debut").value
+            tabDateIn = dateIn.split("-");
+            flagDateIn = true;
+            if (flagDateIn == true && flagDateOut == true) {
+                deltaAnnee = tabDateOut[0] - tabDateIn[0];
+                deltaMois = tabDateOut[1] - tabDateIn[1];
+                deltaJour = tabDateOut[2] - tabDateIn[2];
+                console.log("L'écart est de " + deltaAnnee + " ans, " + deltaMois + " mois, " + deltaJour + " jour")
+                document.getElementById("ecartDate").innerText = "L'écart est de " + deltaAnnee + " ans, " + deltaMois + " mois, " + deltaJour + " jour";
+            }
+        }
+        document.getElementById("date_fin").onchange = function () {
+            dateOut = document.getElementById("date_fin").value
+            tabDateOut = dateOut.split("-");
+            flagDateOut = true;
+            if (flagDateIn == true && flagDateOut == true) {
+                deltaAnnee = tabDateOut[0] - tabDateIn[0];
+                deltaMois = tabDateOut[1] - tabDateIn[1];
+                deltaJour = tabDateOut[2] - tabDateIn[2];
+                console.log("L'écart est de " + deltaAnnee + " ans, " + deltaMois + " mois, " + deltaJour + " jour")
+                document.getElementById("ecartDate").innerText = "L'écart est de " + deltaAnnee + " ans, " + deltaMois + " mois, " + deltaJour + " jour";
+            }
+        }
 
+        document.getElementById("heure_debut").onchange = function () {
+            timeIn = document.getElementById("heure_debut").value
+            console.log(timeIn)
+        }
     }
 }
